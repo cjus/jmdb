86385b9e0940bfc5e31a74ccbe3e6f992e2a783f
cjus <Carlos.Justiniano@gmail.com>
2017-12-06 06:18:15 -0500

    Cleanup and refactoring for aync/await

--- index.js
+++ index.js
@@ -4,6 +4,11 @@
 */
 'use strict';
 
+const http = require('http');
+const hydra = require('hydra');
+const jmdb = require('./lib/jmdb');
+let appLogger;
+
 /**
 * Router route list.
 */
@@ -13,8 +18,6 @@ let routeList = [
   '[put]/v1/{serviceName}/*rest',
   '[delete]/v1/{serviceName}/*rest',
 ];
-
-let appLogger;
 
 process.on('cleanup', () => {
   hydra.shutdown()
@@ -45,77 +48,56 @@ process.on('uncaughtException', (err) => {
   process.emit('cleanup');
 });
 
-const http = require('http');
-const hydra = require('hydra');
-const jmdb = require('./lib/jmdb');
-let config = require('./config/config.json');
+let main = async () => {
+  try
+  {
+    const HydraLogger = require('fwsp-logger').HydraLogger;
+    let hydraLogger = new HydraLogger();
+    hydra.use(hydraLogger);
 
-if (!config.hydra) {
-  /**
-  * @summary Start HTTP server and add request handler callback.
-  * @param {object} request - Node HTTP request object
-  * @param {object} response - Node HTTP response object
-  */
-  let server = http.createServer((request, response) => {
-    jmdb.routeRequest(request, response);
-  });
-  server.listen(config.servicePort);
-  let logEntry = `Starting jmdb service on ${config.servicePort}`;
-  console.log(logEntry);
-  jmdb.init(config, {});
-}
+    let newConfig = await hydra.init(`${__dirname}/config/config.json`, false);
 
-if (config.hydra) {
-  const HydraLogger = require('fwsp-logger').HydraLogger;
-  let hydraLogger = new HydraLogger();
-  hydra.use(hydraLogger);
-
-  hydra.init(`${__dirname}/config/config.json`, false)
-    .then((newConfig) => {
-      config = newConfig;
-      return hydra.registerService();
-    })
-    .then((serviceInfo) => {
-      let logEntry = `Starting jmdb service ${serviceInfo.serviceName}:${hydra.getInstanceVersion()} on ${serviceInfo.serviceIP}:${serviceInfo.servicePort}`;
-      console.log(logEntry);
-      hydra.sendToHealthLog('info', logEntry);
-
-      if (global.gc) {
-        global.gc();
-      } else {
-        console.warn('No GC hook! Start jmdb using `node --expose-gc index.js`.');
-      }
-
-      appLogger = hydraLogger.getLogger();
-      appLogger.info({
-        msg: logEntry
-      });
-
-      hydra.on('log', (_entry) => {
-      });
-
-      /**
-      * @summary Start HTTP server and add request handler callback.
-      * @param {object} request - Node HTTP request object
-      * @param {object} response - Node HTTP response object
-      */
-      let server = http.createServer((request, response) => {
-        jmdb.routeRequest(request, response);
-      });
-      server.listen(serviceInfo.servicePort);
-      let updatedRouteList = [];
-      routeList.forEach((route) => {
-        updatedRouteList.push(route.replace('{serviceName}', serviceInfo.serviceName));
-      });
-      return hydra.registerRoutes(updatedRouteList);
-    })
-    .then(() => {
-      jmdb.init(config, appLogger);
-      return null; // to silence promise warning: http://goo.gl/rRqMUw
-    })
-    .catch((err) => {
-      let stack = err.stack;
-      console.log(stack); // console log because appLogger isn't available in this case.
-      process.emit('cleanup');
+    appLogger = hydraLogger.getLogger();
+    appLogger.info({
+      msg: logEntry
     });
+
+    let serviceInfo = await hydra.registerService();
+
+    // let newConfig = hydraExpress.getRuntimeConfig();
+
+    let logEntry = `Starting jmdb service ${serviceInfo.serviceName}:${hydra.getInstanceVersion()} on ${serviceInfo.serviceIP}:${serviceInfo.servicePort}`;
+    console.log(logEntry);
+    hydra.sendToHealthLog('info', logEntry);
+
+    if (global.gc) {
+      global.gc();
+    } else {
+      console.warn('No GC hook! Start jmdb using `node --expose-gc index.js`.');
+    }
+
+    hydra.on('log', (_entry) => {});
+
+    /**
+    * @summary Start HTTP server and add request handler callback.
+    * @param {object} request - Node HTTP request object
+    * @param {object} response - Node HTTP response object
+    */
+    let server = http.createServer((request, response) => {
+      jmdb.routeRequest(request, response);
+    });
+    server.listen(serviceInfo.servicePort);
+    let updatedRouteList = [];
+    routeList.forEach((route) => {
+      updatedRouteList.push(route.replace('{serviceName}', serviceInfo.serviceName));
+    });
+    await hydra.registerRoutes(updatedRouteList);
+    jmdb.init(newConfig, appLogger);
+  } catch (err) {
+    let stack = err.stack;
+    console.log(stack); // console log because appLogger isn't available in this case.
+    process.emit('cleanup');
+  }
 }
+
+main();
